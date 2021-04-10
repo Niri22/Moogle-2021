ceb0f59fbad5344c9d8626b3349e6c2bb67e4e05
Niri22 <66136976+Niri22@users.noreply.github.com>
2021-04-09 21:10:30 -0400

    Add files via upload
    Search Bar for Hp characters

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./App.css";
-// import mondaySdk from "monday-sdk-js";
+import mondaySdk from "monday-sdk-js";
 import "monday-ui-react-core/dist/main.css"
 //Explore more Monday React Components here: https://style.monday.com/
-import {Search} from "monday-ui-react-core"
-import AdvancedSearch from "./AdvancedSearch/AdvancedSearch"
+import Search from "monday-ui-react-core/dist/Search"
+import Dropdown from "monday-ui-react-core/dist/Dropdown";
+import DialogContentContainer from "monday-ui-react-core/dist/DialogContentContainer"
 
-// const monday = mondaySdk();
+const monday = mondaySdk();
 
 class App extends React.Component {
   constructor(props) {
@@ -14,25 +15,93 @@ class App extends React.Component {
 
     // Default state
     this.state = {
-      settings: {},
-      name: "",
+      characters : []
     };
+    this.hpCharacters = [];
   }
 
   componentDidMount() {
-    // TODO: set up event listeners
+    this.loadCharacters();
+  }
+
+loadCharacters = async () => {
+try {
+    const res = await fetch('https://hp-api.herokuapp.com/api/characters');
+    this.hpCharacters = await res.json();
+    // this.displayCharacters(this.hpCharacters);
+    this.setState({
+      characters : this.hpCharacters
+    }) 
+} catch (err) {
+    console.error(err);
+  }
+};
+
+  keyUpHandler(value) {
+    const searchString = value.toLowerCase();
+
+    const filteredCharacters = this.hpCharacters.filter((character) => {
+        return (
+            character.name.toLowerCase().includes(searchString) ||
+            character.house.toLowerCase().includes(searchString) ||
+           character.actor.toLowerCase().includes(searchString)
+        );
+    });
+    this.setState({
+      characters : filteredCharacters
+    })
+    // displayCharacters(filteredCharacters);
   }
 
   render() {
+    // const mockColorOptions = [
+    //   { value: "Search in Boards", label: "Search in Boards", isFixed: true },
+    //   { value: "Search in Tasks", label: "Search in Tasks", isFixed: true },
+    //   { value: "Search in Comments", label: "Search in Comments", isFixed: true},
+    //   { value: "Search in Teams", label: "Search in Teams", isFixed: true},
+    //   { value: "Search in Everything", label: "Search in Everything", isFixed: true },
+    //   ];
     return <div className="App">
+
+    <div style={{ width: "100%", margin: "10 auto" }}>
+    
+      <h1>&#x2728;Moogle &#x2728;</h1>
+        
       <Search
-        inputAriaLabel={"Search bar"}
+        id="searchBar"
+        inputAriaLabel={"Search for content"}
         autoFocus={true}
-        placeholder={"Search here"}
+        placeholder={"Search Bar"}
+        debounceRate={200}
+        onChange={value => this.keyUpHandler(value)}
+        value={""}
         iconName={"fa-search"}
+        secondaryIconName={"fa-circle"}
+        validation={{
+          None: null,
+          Error: { status: "error" },
+          Success: { status: "success" }
+        }}
+        clearOnIconClick={true}
+        disabled={false}
+        size={Search.sizes.MEDIUM}
       />
-      <AdvancedSearch></AdvancedSearch>
-    </div>;
+      <ul id="charactersList">
+        {
+          this.state.characters
+          .map((character) =>  
+              <li class="character">
+                  <h2>{character.name}</h2>
+                  <p>House: {character.house}</p>
+                  <img src={character.image}></img>
+              </li>
+          )
+        }
+      </ul>
+      
+  </div>
+
+  </div>;
   }
 }
 
