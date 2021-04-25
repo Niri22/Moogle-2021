ed3822fab0bfd44cc9b041478dd2f34eb7022211
Samantha Fassnacht <samantha.fassnacht@gmail.com>
2021-04-25 13:22:33 -0400

    Add top level query creation

--- src/App.js
+++ src/App.js
@@ -3,10 +3,9 @@ import "./App.css";
 // import mondaySdk from "monday-sdk-js";
 import "monday-ui-react-core/dist/main.css"
 //Explore more Monday React Components here: https://style.monday.com/
+import filtersByObjectType from "./util/search_capabilities"
 import {Search} from "monday-ui-react-core"
 import AdvancedSearch from "./AdvancedSearch/AdvancedSearch"
-
-// const monday = mondaySdk();
 
 class App extends React.Component {
   constructor(props) {
@@ -14,8 +13,7 @@ class App extends React.Component {
 
     // Default state
     this.state = {
-      settings: {},
-      name: "",
+      queryString: ''
     };
   }
 
@@ -23,15 +21,51 @@ class App extends React.Component {
     // TODO: set up event listeners
   }
 
+  generateFilterString(objectType, filters) {
+    return filters.filter(filter => filter.type === objectType).reduce((endString, nextFilter) => {
+      return endString + nextFilter.title + ': ' + nextFilter.value + ', '
+    }, '')
+  }
+
+  generateSearchableFieldsString(objectType) {
+    return filtersByObjectType[objectType].searchableFields.reduce((endString, nextField) => {
+      return endString + nextField + "\n"
+    }, '')
+  }
+
+  regenerateQuery(activeFilters) {
+    const newQuery = `{
+      boards (${this.generateFilterString('boards', activeFilters)}) {
+        ${this.generateSearchableFieldsString('boards')}
+      }
+      items (${this.generateFilterString('items', activeFilters)}) {
+        ${this.generateSearchableFieldsString('items')}
+      }
+      tags (${this.generateFilterString('tags', activeFilters)}) {
+        ${this.generateSearchableFieldsString('tags')}
+      }
+      updates (${this.generateFilterString('updates', activeFilters)}) {
+        ${this.generateSearchableFieldsString('updates')}
+      }
+      users (${this.generateFilterString('users', activeFilters)}){
+        ${this.generateSearchableFieldsString('users')}
+      }
+    }`
+    this.setState({
+      queryString: newQuery,
+    })
+  }
+
   render() {
     return <div className="App">
       <Search
+        className="searchBar"
         inputAriaLabel={"Search bar"}
         autoFocus={true}
         placeholder={"Search here"}
         iconName={"fa-search"}
       />
-      <AdvancedSearch></AdvancedSearch>
+      <AdvancedSearch regenerateQuery={this.regenerateQuery.bind(this)}></AdvancedSearch>
     </div>;
   }
 }
