a529ca0418a4f0b358fc0686427956d91a6e2b46
Niri22 <66136976+Niri22@users.noreply.github.com>
2021-04-26 21:15:07 -0400

    Updated DataList.js
    Added display for each Board(name, description, items-name).

--- src/DataList.js
+++ src/DataList.js
@@ -1,23 +1,52 @@
 import React from 'react';
 
+///Problem occurs when Nested List // 
+// First I have a irregular nest List //
+
+// let newList = []
+
+// dataList.map(function mapper(OGlist) {
+//       if (Array.isArray(OGlist)) {
+//             return OGlist.map(mapper);
+//       }
+//       else {
+//             return newList.push(OGlist);
+//       }
+// })
+
 const DataList = ({dataList=[]}) => {
   return (
-    <>
-    {dataList.Boards ? dataList.Boards.map((data,index) => {
-        if (data) {
-          return (
-            <div key={data.Boards}>
-              <h1>{data.Boards.name}</h1>
-              <h1>{data.Boards.description}</h1>
-              <h1>{data.Boards.items}</h1>
-	    </div>
-          	
-    	   )
-    	 }
-    	 return null
-    }) : null }
-    </>
-  );
+    <ul id="BoardList">
+        {
+          dataList.boards ?
+            dataList.boards.map((nextElement) =>  
+              <li class="BoardName">
+                  <h1>{nextElement.name}</h1>
+                  <h2>{nextElement.description}</h2>
+                  <h2> Items: </h2>
+                  <ul id = "Item List">
+                    {nextElement.items ? 
+                      nextElement.items.map((nextElement1) =>
+                      <li class = "ItemName">
+                        <p>{nextElement1.name}</p>
+                        </li>
+                        ) : null}
+                  </ul>
+                  
+              </li>
+          ) : null
+        }
+      </ul>
+  )
 }
+
+{/* {dataList.map(function mapper(OGlist) {
+      if (Array.isArray(OGlist)) {
+            return OGlist.map(mapper);
+      }
+      else {
+            return newList.push(OGlist);
+      }
+      })} */}
 
 export default DataList
